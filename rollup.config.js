d3146e0eaf1d491a9bfec14251fa6bd8f7c97ddc
Jan Brummelte <github@jan-brummelte.de>
2018-04-11 15:18:52 +0200

    Rename bundle files

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,8 @@ import babel from 'rollup-plugin-babel';
 export default {
     input: 'lib/index.js',
     output: [
-        { file: 'build/bundle.js', format: 'cjs' },
-        { file: 'build/bundle.mjs', format: 'es' },
+        { file: 'build/cjs.js', format: 'cjs' },
+        { file: 'build/esm.mjs', format: 'es' },
     ],
     plugins: [resolve(), babel({ exclude: 'node_modules/**' })],
 };
