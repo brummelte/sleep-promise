fb9b198f89c714f9a95f760aa9a668b1a84866ca
Kevin Snyder <kevipapo1@gmail.com>
2018-06-04 14:39:56 -0700

    Add options parameter to sleep function
    `options` is an object that indicates small alterations in function behavior.
    
    The following options are supported:
    
    - `useCachedSetTimeout`: call a reference to the original global `setTimeout` (useful when it gets replaced, e.g. `sinon.useFakeTimers()`, `jest.useFakeTimers()`)
    
    Example:
    
    ```js
    import * as sinon from 'sinon';
    import sleep from 'sleep-promise';
    
    const clock = sinon.useFakeTimers(); // Replace global setTimeout
    
    async(() => {
        await sleep(2000);
        clock.tick(2000);
        console.log('instant, 2 seconds faked by sinon');
    
        await sleep(2000, {
            useCachedSetTimeout: true
        });
        console.log('2 seconds later, called cached global setTimeout before sinon replaced it');
    })();
    ```

--- types/esm.d.ts
+++ types/esm.d.ts
@@ -1,3 +1,5 @@
-declare function sleep<T = any>(timeout: number): Promise<T> & ((value: T) => T);
+import { SleepOptions } from './options';
+
+declare function sleep<T = any>(timeout: number, options: SleepOptions = {}): Promise<T> & ((value: T) => T);
 
 export default sleep;
