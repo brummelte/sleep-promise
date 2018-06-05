6d1294df8d12fcd0c326a7207f80fddce484b6c9
Jan Brummelte <github@jan-brummelte.de>
2018-06-05 07:49:25 +0200

    Refactor code

--- types/esm.d.ts
+++ types/esm.d.ts
@@ -1,5 +1,8 @@
 import { SleepOptions } from './options';
 
-declare function sleep<T = any>(timeout: number, options: SleepOptions = {}): Promise<T> & ((value: T) => T);
+declare function sleep<T = any>(
+    timeout: number,
+    options: SleepOptions = {},
+): Promise<T> & ((value: T) => T);
 
 export default sleep;
