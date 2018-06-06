21959b7f02f87c4d8ba8bd0f385f138e4594c40b
Jan Brummelte <github@jan-brummelte.de>
2018-06-06 10:52:04 +0200

    Add minification

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,9 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import minify from 'rollup-plugin-babel-minify';
 
 export default {
     input: 'lib/index.js',
-    output: [
-        { file: 'build/cjs.js', format: 'cjs' },
-        { file: 'build/esm.mjs', format: 'es' },
-    ],
-    plugins: [resolve(), babel({ exclude: 'node_modules/**' })],
+    output: [{ file: 'build/cjs.js', format: 'cjs' }, { file: 'build/esm.mjs', format: 'es' }],
+    plugins: [resolve(), babel({ exclude: 'node_modules/**' }), minify({ comments: false })],
 };
