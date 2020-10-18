988a828bee6f51d0e5e8173a9cdb3d7a56d36bfc
Jan Brummelte <github@jan-brummelte.de>
2020-10-18 15:44:12 +0200

    refactor: rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,15 @@
+/* eslint-disable filenames/match-exported */
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import minify from 'rollup-plugin-babel-minify';
 
-export default {
+const rollupConfig = {
     input: 'lib/index.js',
-    output: [{ file: 'build/cjs.js', format: 'cjs' }, { file: 'build/esm.mjs', format: 'es' }],
+    output: [
+        { file: 'build/cjs.js', format: 'cjs' },
+        { file: 'build/esm.mjs', format: 'es' },
+    ],
     plugins: [resolve(), babel({ exclude: 'node_modules/**' }), minify({ comments: false })],
 };
+
+export default rollupConfig;
