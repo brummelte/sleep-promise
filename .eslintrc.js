b57ca8919845c8f1f2ab00cb64eff6f937cc0e7e
Jan Brummelte <github@jan-brummelte.de>
2020-10-18 14:35:09 +0200

    refactor: eslint config Settings are now included in base config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,3 @@
 module.exports = {
     extends: '@brummelte/eslint-config',
-    env: { es6: true },
-    overrides: [
-        {
-            files: ['lib/**/*.test.js'],
-            env: { jest: true },
-        },
-    ],
 };
