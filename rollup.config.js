f913cc8bdc07ef1cb31dc2b28c0cc087bbf90479
Jan Brummelte <github@jan-brummelte.de>
2020-10-18 16:40:14 +0200

    chore!: use terser instead of babel minify

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable filenames/match-exported */
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
-import minify from 'rollup-plugin-babel-minify';
+import { terser } from 'rollup-plugin-terser';
 
 const rollupConfig = {
     input: 'lib/main.js',
@@ -9,7 +9,7 @@ const rollupConfig = {
         { file: 'build/cjs.js', format: 'cjs' },
         { file: 'build/esm.mjs', format: 'es' },
     ],
-    plugins: [resolve(), babel({ exclude: 'node_modules/**' }), minify({ comments: false })],
+    plugins: [resolve(), babel({ exclude: 'node_modules/**' }), terser()],
 };
 
 export default rollupConfig;
